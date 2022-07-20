3369a51d51873735c039721513c765d07deda9ee
Catarina Alves <catarina._3@hotmail.com>
2022-07-20 18:42:53 -0500

    Add support for predictions from the CNN or RNN alone

--- Network/predict.py
+++ Network/predict.py
@@ -9,8 +9,16 @@ import numpy as np
 def predict(network, dataset):
     network.eval()
 
-    predictions = torch.max(network(dataset[:]['lightcurve'],
-                                    dataset[:]['image']), 1)[1].data.numpy()
+    try:
+        predictions = torch.max(network(dataset[:]['lightcurve'],
+                                        dataset[:]['image']), 1)[1].data.numpy()
+    except TypeError:  # Instead of ZIPPER, we are using CNN or RNN
+        try:  # RNN
+            predictions = torch.max(network(dataset[:]['lightcurve']),
+                                    1)[1].data.numpy()
+        except TypeError:  # CNN
+            predictions = torch.max(network(dataset[:]['image']),
+                                    1)[1].data.numpy()
     labels = dataset[:]['label'].data.numpy()
 
     print("Accuracy:", np.sum(predictions == labels) / len(labels))
