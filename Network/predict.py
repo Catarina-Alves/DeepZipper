5c90097ab01b8b07f8780b6829877747d63d3a2f
Catarina Alves <catarina._3@hotmail.com>
2022-07-20 18:45:38 -0500

    Include a brief description of the function

--- Network/predict.py
+++ Network/predict.py
@@ -7,11 +7,25 @@ import numpy as np
 
 
 def predict(network, dataset):
+    """
+    Use the network to make predictions about the dataset.
+
+    This function works with the network types ZIPPER, RNN, CNN.
+
+    Args:
+        network
+        dataset: dataset to make predictions about
+
+    Returns:
+        predictions
+        labels
+    """
     network.eval()
 
     try:
         predictions = torch.max(network(dataset[:]['lightcurve'],
-                                        dataset[:]['image']), 1)[1].data.numpy()
+                                        dataset[:]['image']),
+                                1)[1].data.numpy()
     except TypeError:  # Instead of ZIPPER, we are using CNN or RNN
         try:  # RNN
             predictions = torch.max(network(dataset[:]['lightcurve']),
