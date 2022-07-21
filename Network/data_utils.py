e6a92f3bf68bf2a2f245fb233f1a0657010abefb
Catarina Alves <catarina._3@hotmail.com>
2022-07-21 14:21:04 -0500

    More pep8 compliant

--- Network/data_utils.py
+++ Network/data_utils.py
@@ -1,16 +1,17 @@
 """
 Dataset and Dataloader objects for DeepTransients
 """
-
-import glob
+# import glob
 
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from torchvision import transforms
+# import pandas as pd
 import torch
+
+from sklearn.model_selection import train_test_split
+# from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
-#from torch.utils.data import TensorDataset
+# from torch.utils.data import TensorDataset
+
 
 class CombinedDataset(Dataset):
     """Dataset of DeepLenstronomy Lightcurves and Images"""
@@ -34,27 +35,29 @@ class CombinedDataset(Dataset):
         image = self.images[idx]
         lightcurve = self.lightcurves[idx]
         label = np.array(self.labels[idx])
-        
+
         sample = {'lightcurve': lightcurve, 'image': image, 'label': label}
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample
-    
+
+
 class ToCombinedTensor(object):
     """Convert ndarrays in sample to Tensors."""
 
     def __call__(self, sample):
-        lightcurve, image, label = sample['lightcurve'], sample['image'], sample['label']
+        lightcurve, image, label = (sample['lightcurve'], sample['image'],
+                                    sample['label'])
 
         return {'lightcurve': torch.from_numpy(lightcurve).float(),
                 'image': torch.from_numpy(image).float(),
                 'label': torch.from_numpy(label)}
 
 
-
-def make_train_test_datasets(directory: str, class_names: list, suffix: int, transform=ToCombinedTensor(), label_map={}):
+def make_train_test_datasets(directory: str, class_names: list, suffix: int,
+                             transform=ToCombinedTensor(), label_map={}):
     images, lightcurves, labels, metadata = [], [], [], []
 
     # Ingest and label data
@@ -64,17 +67,18 @@ def make_train_test_datasets(directory: str, class_names: list, suffix: int, tra
         lc = np.load(lc_file)
         print(lc.shape)
         lightcurves.append(lc)
-        
+
         # Images
         im_file = f'{directory}/{class_name}_ims_{suffix}.npy'
         im = np.load(im_file)
         images.append(im.reshape((len(im), 4, 45, 45)))
-        
+
         # Labels
-        class_label = label_map[class_name] if class_name in label_map else label
+        class_label = (label_map[class_name] if class_name in label_map
+                       else label)
         print(class_label)
         labels.extend([class_label]*len(im))
-        
+
         # Metadata
         md_file = f'{directory}/{class_name}_mds_{suffix}.npy'
         metadata.append(np.load(md_file, allow_pickle=True).item())
@@ -83,33 +87,37 @@ def make_train_test_datasets(directory: str, class_names: list, suffix: int, tra
     X_lc = np.concatenate(lightcurves)
     X_im = np.concatenate(images)
     y = np.array(labels, dtype=int)
-    train_lightcurves, test_lightcurves, train_labels, test_labels = train_test_split(
+    (train_lightcurves, test_lightcurves,
+     train_labels, test_labels) = train_test_split(
         X_lc, y, test_size=0.1, random_state=6, stratify=y)
-    
+
     train_images, test_images, garb1, garb2 = train_test_split(
         X_im, y, test_size=0.1, random_state=6, stratify=y)
-    
+
     # Split and save metadata
     full_md = []
     for md in metadata:
         for v in md.values():
             full_md.append(v)
-    
+
     train_md, test_md, garb1, garb2 = train_test_split(
         full_md, y, test_size=0.1, random_state=6, stratify=y)
-    
+
     train_md = {idx: train_md[idx] for idx in range(len(train_md))}
     test_md = {idx: test_md[idx] for idx in range(len(test_md))}
-    
-    np.save(f"{directory}/{directory}_train_md_{suffix}.npy", train_md, allow_pickle=True)
-    np.save(f"{directory}/{directory}_test_md_{suffix}.npy", test_md, allow_pickle=True)
-    
-    
+
+    np.save(f"{directory}/{directory}_train_md_{suffix}.npy", train_md,
+            allow_pickle=True)
+    np.save(f"{directory}/{directory}_test_md_{suffix}.npy", test_md,
+            allow_pickle=True)
+
     # Create a PyTorch Dataset
-    return (CombinedDataset(train_images, train_lightcurves, train_labels, transform=transform),
-            CombinedDataset(test_images, test_lightcurves, test_labels, transform=transform))
-        
+    return (CombinedDataset(train_images, train_lightcurves, train_labels,
+                            transform=transform),
+            CombinedDataset(test_images, test_lightcurves, test_labels,
+                            transform=transform))
+
 
 def make_dataloader(dataset, batch_size=5, shuffle=True, num_workers=4):
-    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
+                      num_workers=num_workers)
